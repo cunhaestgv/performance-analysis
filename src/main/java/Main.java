6d1dd39f171f8a61283ddcf5729869b79d669f0f
Carlos Cunha <cunha.cas@gmail.com>
2021-01-18 11:19:11 +0000

    First

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import harreader.HarReaderException;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException, HarReaderException {
+	    HarFileModel model = new HarFileModel();
+    }
+}
